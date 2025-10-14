ee37bb807f89f33213488a8a1063159eab961d1f
AlvaroCarrion <alvarocarrion.git@gmail.com>
2025-10-14 17:45:03 +0200

    creadas algunas clases

---
+++ Proyecto1/src/app/class/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Class;
+//Me falta que me inyecte automáticamente el use (namespace) de la clase enum de TipoUsuario.. si no.. es que está mal hecha la clase.
+
+class User
+{
+    private string $username;
+    private string $password;
+    private string $email;
+    private  int $edad;
+    private array $visualizaciones;
+    private TipoUsuario $tipo;
+}
---
+++ Proyecto1/src/enum/TipoUsuario.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\enum;
+
+enum TipoUsuario
+{
+    case NORMAL;
+    case ANUNCIOS;
+    case ADMIN;
+}
